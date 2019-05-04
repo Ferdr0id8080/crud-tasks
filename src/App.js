651574220609e6ed0b34f99b4da14558b3ee1668
Andres Fernando <andres.fernando08@gmail.com>
2019-05-04 16:41:29 -0500

    importado react-router y se enlazaron los componetes NewTask y Tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
 
 import Toolbar from './components/Toolbar/Toolbar'
+import Tasks from './containers/Tasks/Tasks'
+import NewTask from './containers/NewTask/NewTask'
 
 
-function App() {
+const app = () => {
 	return (
 		<div className="App">
 			<Toolbar />
-			
+			<Switch>
+				<Route path='/new' component={NewTask} />
+                <Route exact path='/' component={Tasks} />
+			</Switch>
 		</div>
 	);
 }
 
-export default App;
+export default app;
