6f63fcaff1702be0c5c8b53c582a2ae787d0a924
Andres Fernando <andres.fernando08@gmail.com>
2019-05-04 23:34:42 -0500

    enlazado los componentes de inputs y boton al NewTask y se implementaron las llamadas al api a traves de axios

--- src/containers/NewTask/NewTask.js
+++ src/containers/NewTask/NewTask.js
@@ -2,18 +2,77 @@ import React, { Component } from 'react'
 
 import styles from './NewTask.module.css'
 
+import axios from '../../helpers/axios'
+
+import Input from '../../components/Input/Input'
+import Button from '../../components/Button/Button'
+
+
 class NewTask extends Component {
-    state = {}
+    state = {
+        form: {
+            title: '',
+            description: ''
+        }
+    }
+
+    inputChangedHandler = (e, input) => {
+        const value = e.target.value
+        const newFormValues = {
+            ...this.state.form,
+            [input]: value
+        }
+        this.setState({form: newFormValues})
+    }
+
+    formSubmittedHandler = ev => {
+        ev.preventDefault()
+
+        const data = {
+            title: this.state.form.title.trim(),
+            description: this.state.form.description.trim()
+        }
+
+        if (data.title.length === 0) {
+            alert('El campo de [title] es requerido')
+            return
+        }
+
+        if (data.description.length === 0) {
+            alert('El campo de [description] es requerido')
+            return 
+        }
+        
+        axios.post('/tasks/create', data)
+             .then(res => {
+                 if (res.data.success) {
+                     const clearedForm = {
+                         ...this.state.form,
+                         title: '',
+                         description: ''
+                     }
+                     this.setState({form: clearedForm})
+                     alert('Tarea agregada exitosamente')
+                 }
+             })
+             .catch(err => {
+                 console.log(err)
+             })
+
+    }
+
 
     render() {
         return (
             <div className={styles.NewTask}>
-                <form>
-                    <fieldset>
-                        <legend>Nueva Tarea</legend>
-                        <input type="text" placeholder='Titulo' /><br />
-                        <input type="text" placeholder='Descripcion' /><br />
-                        <button>Añadir</button>
+                <form className={styles.formTask} onSubmit={this.formSubmittedHandler}>
+                    <fieldset className={styles.fieldSetTask}>
+                        <legend>Creando una nueva tarea</legend>
+                        <Input label='Titulo' inputtype='text' 
+                            value={this.state.form.title} changed={e => this.inputChangedHandler(e, 'title')} />
+                        <Input label='Descripcion' inputtype='text' 
+                            value={this.state.form.description} changed={e => this.inputChangedHandler(e, 'description')} />
+                        <Button>Añadir</Button>
                     </fieldset>
                 </form>
             </div>
