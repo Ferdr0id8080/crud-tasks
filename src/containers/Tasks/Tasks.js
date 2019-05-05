1e4b9ee4adc941ff437bec1bf4be697f25502e32
Andres Fernando <andres.fernando08@gmail.com>
2019-05-05 11:46:51 -0500

    se cambio la lista de tareas por una tabla y se agregaron los estilos css correspondientes

--- src/containers/Tasks/Tasks.js
+++ src/containers/Tasks/Tasks.js
@@ -7,14 +7,18 @@ import styles from './Tasks.module.css'
 
 class Tasks extends Component {
     state = {
-        tasks: []
+        tasks: [],
+        loading: true
     }
 
     componentDidMount() {
         axios.get('/tasks')
              .then(res => {
                  if (res.data.success) {
-                     this.setState({tasks: res.data.data})
+                     this.setState({
+                        tasks: res.data.data,
+                        loading: false
+                    })
                  }
              })
              .catch(err => {
@@ -25,15 +29,36 @@ class Tasks extends Component {
     render() {
         return (
             <div className={styles.Tasks}>
-                <h2>Listado de Tareas</h2>
-                <ul>
-                    {this.state.tasks.length > 0 && this.state.tasks.map(i => (
-                        <li key={i._id}>
-                            {i.title} => {i.description}
-                        </li>
-                    ))
-                    }
-                </ul>
+                <h2 style={{margin: '8px 0px'}}>Listado de Tareas</h2>
+                {this.state.loading
+                    ?
+                        <p>Cargando...</p>
+                    : 
+                        this.state.tasks.length > 0 
+                            ?
+                                (
+                                    <table className={styles.tableTasks}>
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Titulo</th>
+                                                <th>Descripcion</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.tasks.map((i, idx) => (
+                                                <tr key={i._id}>
+                                                    <td>{idx + 1}</td>
+                                                    <td>{i.title}</td>
+                                                    <td>{i.description}</td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                )
+                            : 
+                                <p>No hay ninguna tarea registrada</p>
+                }
             </div>
         )
     }
